9ef2be1297679e4156aedbeee05c183e6f6091c4
Jkthirumal <jkthirumal@gmail.com>
2020-03-12 21:09:18 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,103 +1,98 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById("app");
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 var acctData = [
   {
-    "acctNum":"AAA-1234",
-    "user":"Alice"
+    acctNum: "AAA-1234",
+    user: "Alice"
   },
   {
-    "acctNum":"AAA-4652",
-    "user":"Bob"
+    acctNum: "AAA-4652",
+    user: "Bob"
   },
   {
-    "acctNum":"AAA-7895",
-    "user":"Alice"
+    acctNum: "AAA-7895",
+    user: "Alice"
   },
   {
-    "acctNum":"AAA-5896",
-    "user":"Bob"
+    acctNum: "AAA-5896",
+    user: "Bob"
   },
   {
-    "acctNum":"AAA-5684",
-    "user":"Alice"
+    acctNum: "AAA-5684",
+    user: "Alice"
   }
 ];
 
 var balance = [
   {
-    "AAA-1234" : "4582.25"
+    "AAA-1234": "4582.25"
   },
   {
-    "AAA-4652" : "25554.25"
+    "AAA-4652": "25554.25"
   },
   {
-    "AAA-7895" : "7596.25"
+    "AAA-7895": "7596.25"
   },
   {
-    "AAA-5896" : "254555.25"
+    "AAA-5896": "254555.25"
   },
   {
-    "AAA-5684" : "254444.25"
+    "AAA-5684": "254444.25"
   }
 ];
-var userObj = {
-  accInfo: acctData,
-  bal: balance
-}
-//user
-//sortBy
-//sortDirection
-// var obj = {
-//   user: function(name) {
-//     console.log(acctData.filter(item=> { return item.user === name; }));
-//   },
-//   ,
-  // Given values for accData and balances below, write a function that returns only an array of 
-  // account number, and accepts the fullowing optional params
-  // user, sortBy("acctNum," or "balance");
-  // sortDirection("asc"/"dsc", default to asc)
-// }
-function soryBy(sortType) {
-    console.log(acctData.filter(item=> { return item.user === name; }));
-  }
-function sortDirection(direction = 'asc') {
-    switch(direction) {
-      case 'asc':
-        console.log(acctData.sort((a,b) => (a.user < b.user) ? 1 : ((b.user < a.user) ? -1 : 0)));
-      break;
-      case 'desc':
-        console.log(acctData.sort((a,b) => (a.user < b.user) ? 1 : ((b.user < a.user) ? -1 : 0)));
-      break;
-      default:
-        console.log('No sort type mentioned.');
-      break;
-    }
-  }
-  var userData;
-function getAccNum(userName, sortBy, sortDirection){
-  userData = ( typeof userName === "string")? displayUserDetails(userName): [];
-  ( typeof sortBy === "function")? sortBy(userData):''; 
-  
-  return acctData.map(item=>{ return item.acctNum});
 
-}
-function sortBy(key) {
-   console.log(userData.filter(item=> { return item[key] === name; }));
+function getAccNum(userName, filterByName, direction) {
+  var userData =
+    typeof userName === "string" ? displayUserDetails(userName) : [];
+  var filterdData =
+    typeof filterByName === "string" ? sortBy(filterByName) : "";
+  var finalRes = typeof direction === "string" ? sortDirection(direction) : "";
+  // console.log(filterdData);
+  function sortBy(key) {
+    return userData.map(item => {
+      return item[filterByName];
+    });
+  }
+  function sortDirection(direction = "asc") {
+    var finalData = [];
+    switch (direction) {
+      case "asc":
+        finalData = filterdData.sort((a, b) => (a < b ? 1 : b < a ? -1 : 0));
+        break;
+      case "desc":
+        finalData = filterdData.sort((a, b) => (a < b ? 1 : b < a ? -1 : 0));
+        break;
+      default:
+        console.log("No sort type mentioned.");
+        break;
+    }
+    finalData.map(item => {
+      console.log(item);
+    });
+  }
+  return acctData.map(item => {
+    return item.acctNum;
+  });
 }
 
 function displayUserDetails(userName) {
-  return acctData.filter(item=> { return item.user === userName; }).map(user=>{
-    let result = {...{balance:balance.find(item=> item[user.acctNum])[user.acctNum]}, ...user};
-    return result;
-  });
+  return acctData
+    .filter(item => {
+      return item.user === userName;
+    })
+    .map(user => {
+      let result = {
+        ...{ balance: balance.find(item => item[user.acctNum])[user.acctNum] },
+        ...user
+      };
+      return result;
+    });
 }
- 
-getAccNum('Alice',sortBy("acctNum"));
- //sortDirection();
 
-
+getAccNum("Alice", "acctNum", "asc");
+//sortDirection();
